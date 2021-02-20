db4ebd75c7ecb29c61435e685d5a0d4fd681bc15
mattmck <matthew.mckerroll@gmail.com>
2021-02-20 15:33:17 -0500

    added logic for the pending db to store transactions while offline

--- public/db.js
+++ public/db.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 let db;
 
 //create indexed db
@@ -9,19 +11,53 @@ request.onupgradeneeded = function(event) {
 }
 
 request.onsuccess = function(event){
-
+    db = event.target.result;
+    if (navigator.onLine){
+        checkForPending()
+    }
 }
 
 request.onerror = function(event){
-
+    console.log("There was an error:" + event.target.errorCode);
 }
 
 function saveRecord(record){
+    const transaction = db.transaction(["pending"], "readwrite");
 
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+  
+    // add record to your store with add method.
+    store.add(record);
 }
 
 function checkForPending() {
 
+    const transaction = db.transaction(["pending"], "readwrite");
+    const storage = transaction.objectStore('pending');
+    const getAll = storage.getAll();
+
+    getAll.onsuccess = function(){
+        if (getAll.result.length>0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+                }
+              })
+              .then(response => response.json())
+              .then(()=>{
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                const storage = transaction.objectStore("pending");
+
+                storage.clear();
+              })
+        }
+    }
+
 }
 
-window.addEventListener("online", checkDatabase);
+window.addEventListener("online", checkForPending);
